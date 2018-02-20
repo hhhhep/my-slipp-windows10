b913195c197a03a57f5bfc8c2a2d1e06b9b0405d
hep <hhhhep@gmail.com>
2018-02-20 18:18:27 +0900

    소스 코드 수정

--- src/main/java/net/slipp/HelloWorldController.java
+++ src/main/java/net/slipp/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@GetMapping("/helloworld")
 	public String helloworld() {
-		return "Hello World!";
+		return "Hello World! Hi!";
 	}
 }
